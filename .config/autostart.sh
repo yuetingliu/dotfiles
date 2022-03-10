3b961be059fe8e9856ec2fea880d73b859c8a7c2
Yueting Liu <yueting1106.liu@gmail.com>
2022-03-10 23:40:56 +0100

    add picom and desktop entries

---
+++ .config/autostart.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# window compositor
+picom -b
+
+# set up dual display
+#if [[ -f /home/yueting/.screenlayout/arrange.sh ]]; then
+#    source $HOME/.screenlayout/arrange.sh
+#fi
+#xrandr --output eDP1 --auto --primary --output DP2 --auto --same-as eDP1
+#/usr/bin/xrandr --output DP2 --auto --right-of eDP1 --scale 1.5
+#/usr/bin/xrandr --output eDP1 --auto --primary --mode 1920x1200 --output DP2 --auto --mode 3440x1440 --right-of eDP1 --scale 1
+#autorandr -c
+
+# set background on startup
+nitrogen --restore &
+
+# autostart applications for system level and user level
+XDG_DIR=/etc/xdg/autostart/
+cd $XDG_DIR
+dex *.desktop &
+DIR=$HOME/.config/autostart/
+cd $DIR
+dex *.desktop &
